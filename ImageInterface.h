93a60b9e96aee46cb14d92b4a883cdd6fb361e65
Sergey <st142081@student.spbu.ru>
2025-12-19 04:40:50 +0300

    create Image Interface

---
+++ ImageInterface.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+class ImageInterface
+{
+public:
+    virtual bool readFromFile(const std::string& path) = 0;
+    virtual bool writeToFile(const std::string& path) = 0;
+
+    virtual void rotate90Cw() = 0;
+    virtual void rotate90CCw() = 0;
+
+    virtual void Gauss() = 0;
+
+    virtual ~ImageInterface() = default;
+};
